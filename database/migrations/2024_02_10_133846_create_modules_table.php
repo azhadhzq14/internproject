fbc7770701a7b68b5abf64a6391e8629b467a7d7
xxMrPHDxx <pian.minecraft1@gmail.com>
2024-02-10 22:41:55 +0800

    added table for prasekolah & sekren

---
+++ database/migrations/2024_02_10_133846_create_modules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('modules', function (Blueprint $table) {
+      $table->id();
+      $table->string('kod_module');
+      $table->string('nama_module');
+      $table->string('bil_ms');
+      $table->string('jenis_bahasa');
+      $table->unsignedBigInteger('paper_kategori_subjek_id');
+      $table->unsignedBigInteger('author_id');
+      $table->timestamps();
+
+      $table->foreign('paper_kategori_subjek_id')->references('id')->on('paper_kategori_subjek')->onDelete('cascade');
+      $table->foreign('author_id')->references('id')->on('author')->onDelete('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('modules');
+  }
+};
